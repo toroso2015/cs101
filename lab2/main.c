b9fd98ab5ab7b950f9c6f70ff0e98f305dced5b3
toroso2015 <zxcasd881126@gmail.com>
2022-03-17 17:06:44 +0800

    lab2

--- lab2/main.c
+++ lab2/main.c
@@ -1,38 +1,4 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <string.h>
-
-int main(){
-    FILE* fp_bin;
-    FILE* fp_txt;
-    FILE* oper_bin ;
-    FILE* record_bin;
-
-    typedef struct lotto_record {
-    int lotto_no;
-    int lotto_receipt;
-    int emp_id;
-    char YMD[32];
-    char HMS[32];
-    }lotto_record_t;
-
-    time_t now = time(0);
-    strftime (YMD,100,"%Y%m%d",localtime(&now));
-    strftime (HMS,100,"%H:%M:%S",localtime(&now));
-    
-
-    int count[]={1};
-    oper_bin=fopen("emp_id.bin","wb+");
-    if((fp_bin=fope#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
@@ -137,77 +103,6 @@ int main(){
     fp_record = fopen("records.bin","ab");
     fwrite(&record, sizeof(lotto_record_t), 1, fp_record);
     fclose(fp_record);
-
-    count[0]++;
-    fp_bin=fopen("count.bin","wb+");
-    fwrite(count,sizeof(count),1,fp_bin);
-    fclose(fp_bin);
-}
-f(count), 1 ,fp_bin);
-        fclose(fp_bin);
-    }
-    else{
-        fp_bin=fopen("count.bin","wb+");
-        fwrite(count, sizeof(count), 1, fp_bin);
-        fclose(fp_bin);
-    }
-
-    fclose(oper_bin);
-    
-    time_t curtime;
-    time(&curtime);
-    int arr_num[5][7],group,flag,temp,userID;
-    srand((unsigned) time(NULL));
-    printf("請輸入操作人員ID(0~5):");
-    scanf("%d",&userID);
-    printf("歡迎光臨長庚樂透彩購買機台,請問您要買幾組樂透彩:");
-    scanf("%d",&group);
-
-    char filename[50];
-    snprintf(filename, sizeof(filename), "lotto[%04d].txt",count[0]);
-    fp_txt = fopen(filename,"w+");
-    fprintf(fp_txt,"========= lotto649 =========\n");
-    fprintf(fp_txt,"========+ No.%05d +========\n",count[0]);
-    fprintf(fp_txt,"= %.*s =\n", 24, ctime(&curtime));
-    for(int i=0; i<5; i++){
-        if(group>=(i+1)){
-            for(int j=0; j<=6;){
-                do{
-                    j!=6? (arr_num[i][j] = rand()%69+1) : (arr_num[i][j] = rand()%10+1);
-                    flag=0;
-                    for(int k=0; k<j; k++){
-                        temp=0;
-                        if(arr_num[i][j]==arr_num[i][k]){
-                            flag=1;
-                        }
-                        else if(j<5 && arr_num[i][j]<arr_num[i][k]){
-                            temp=arr_num[i][j];
-                            arr_num[i][j]=arr_num[i][k];
-                            arr_num[i][k]=temp;
-                        }
-                    }
-                }while(flag==1);
-                j++;
-            }
-        }
-    }
-
-    for(int i=0; i<5; i++){
-        fprintf(fp_txt,"[%d] : ",i+1);
-        for(int j=0; j<=6; j++){
-            if(group>=(i+1)){
-                fprintf(fp_txt,"%02d ",arr_num[i][j]);
-            }
-            else{
-                fprintf(fp_txt,"-- ");
-            }
-        }
-        fprintf(fp_txt,"\n");
-    }
-     fprintf(fp_txt,"==========Op.0000%d =========\n",userID);
-    fprintf(fp_txt,"========= csie@CGU =========\n");
-    printf("已為您購買的%d組樂透組合輸出至%s\n",group,filename);
-    fclose(fp_txt);
 
     count[0]++;
     fp_bin=fopen("count.bin","wb+");
