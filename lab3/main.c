2fd5c03b9c946dd626ec9b116b0b9e09148efcdf
toroso2015 <zxcasd881126@gmail.com>
2022-03-31 17:07:49 +0800

    lab3

--- lab3/main.c
+++ lab3/main.c
@@ -17,9 +17,25 @@ int rec_strlen(char* s){
     return rec_strlen(s+1)+1;
 }
 
+char* my_sort(char* s) {
+	int len = 0;
+	for(int i = 0; *(s + i) != '\0'; i++) len++;
+	for(int i = 0; i < len; i++) {
+		for(int j = 0; j < len - 1 - i; j++) {
+			if(*(s + j) > *(s + j + 1)) {
+				char temp = *(s + j);
+					*(s + j) = *(s + j + 1);
+					*(s + j + 1) = temp;
+			}
+		}
+	}
+	return s;
+}
+
 int main(){
     char s[]="IU123456789";
     printf("len=%d\n",my_strlen(s));
     printf("len=%d\n",rec_strlen(s));
+    printf("sort = %s\n", my_sort(s));
     return 0;
 }
