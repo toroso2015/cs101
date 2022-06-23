09da2445002f9f23c5d74a2bceb874ee20bba63d
toroso2015 <91246873+toroso2015@users.noreply.github.com>
2022-06-23 17:02:19 +0800

    Update main.cpp

--- fin4/main.cpp
+++ fin4/main.cpp
@@ -1,33 +1,36 @@
 #include <iostream>
+#include <cstdio> 
 #include <string>
-#include <ctype.h>
-
+#include <cctype>
 using namespace std;
+
 class freqFile{
-	public:
-		string filename;
-		FILE* fp;
-		int cnt[26];
-		freqFile(string filename){
-			this->filename = filename;
-			for(int i = 0 ; i<26;i++){
-				cnt[i] = 0;
-			}
-		}
-		void readFile(){
-			fp = fopen(filename.c_str(), "r");
-			char c;
-			while(fscanf(fp,"%c",&c)!=EOF){
-				c = tolower(c);
-				cnt[c-'a']++;
-			}
-		}
-		void showFreq(){
-			for(int i = 0 ; i<26;i++){
-				printf("[%c] = %d\n", i+'a', cnt[i]);  
-			}
-		}
+    public:
+        string filename;
+        FILE* fp;
+        int count[26];
+        freqFile(string filename){
+            this->filename = filename;
+            for(int i = 0 ; i < 26; i++){
+                count[i] = 0;
+            }
+        }
+        void readFile(){
+            fp = fopen(filename.c_str(), "r");
+            char c;
+            while(fscanf(fp,"%c",&c)!=EOF){
+                c = tolower(c);
+                count[c-'a']++;
+            }
+            fclose(fp);
+        }
+        void showFreq(){
+            for(int i = 0 ; i < 26; i++){
+                cout << "["<< char(i+'a') << "] = " << count[i] << endl;  
+            }
+        }
 };
+
 int main()
 {
     freqFile myFile("main.cpp");
